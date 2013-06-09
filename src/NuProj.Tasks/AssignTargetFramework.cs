97d08deba1ae847f3923e651bbe8d3819041f383
Immo Landwerth <immo@landwerth.net>
2013-06-08 17:23:51 -0700

    Add basic support <ProjectReference> elements

---
+++ src/NuProj.Tasks/AssignTargetFramework.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Versioning;
+
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+using NuGet;
+
+namespace NuProj.Tasks
+{
+    public class AssignTargetFramework : Task
+    {
+        [Required]
+        public ITaskItem[] OutputsWithTargetFrameworkInformation { get; set; }
+
+        [Output]
+        public ITaskItem[] Libraries { get; set; }
+
+        public override bool Execute()
+        {
+            var seenPackagePaths = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            Libraries = (from item in OutputsWithTargetFrameworkInformation.Select(ConvertToLibrary)
+                         let packagePath = item.GetMetadata("PackagePath")
+                         where seenPackagePaths.Add(packagePath)
+                         select item).ToArray();
+
+            return true;
+        }
+
+        private static ITaskItem ConvertToLibrary(ITaskItem output)
+        {
+            var fileName = output.ItemSpec;
+            var frameworkName = new FrameworkName(output.GetMetadata("TargetFrameworkMoniker"));
+            var frameworkString = VersionUtility.GetShortFrameworkName(frameworkName);
+            var metadata = output.CloneCustomMetadata();
+            metadata["TargetFramework"] = frameworkString;
+            metadata["PackagePath"] = Path.Combine(frameworkString, Path.GetFileName(fileName));
+            return new TaskItem(fileName, metadata);
+        }
+    }
+}
