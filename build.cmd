b780338ec561b91cea426d6977bcaeaab7d9c96f
Immo Landwerth <immo@landwerth.net>
2014-09-21 07:41:46 -0700

    Fix typo in build.cmd

--- build.cmd
+++ build.cmd
@@ -2,7 +2,7 @@
 
 :: Note: We've disabled node reuse because it causes file locking issues.
 ::       The issue is that we extend the build with our own targets which
-::       means that that rebuilding cannot successully delete the task
+::       means that that rebuilding cannot successfully delete the task
 ::       assembly.
 
 "%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe" "%~dp0\build.proj" /nologo /m /v:m /nr:false /flp:verbosity=normal %1 %2 %3 %4 %5 %6 %7 %8 %9
