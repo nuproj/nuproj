39be4831af241487a91d8466de75fcd30a50a501
Andrew Arnott <andrewarnott@gmail.com>
2014-09-29 08:11:48 -0700

    Remove unused project tree factory.
    Not only is it unused, tree modifiers are not generally privileged to create new nodes. So it's a bad import to have.

--- src/NuProj.Package/NuProjProjectTreeModifier.cs
+++ src/NuProj.Package/NuProjProjectTreeModifier.cs
@@ -11,9 +11,6 @@ namespace NuProj.ProjectSystem
     [PartMetadata(ProjectCapabilities.Requires, NuProjCapabilities.NuProj)]
     internal sealed class NuProjProjectTreeModifier : IProjectTreeModifier
     {
-        [Import]
-        public Lazy<IProjectTreeFactory> TreeFactory { get; set; }
-
         public IProjectTree ApplyModifications(IProjectTree tree, IProjectTreeProvider projectTreeProvider)
         {
             if (tree.Capabilities.Contains(ProjectTreeCapabilities.ProjectRoot))
