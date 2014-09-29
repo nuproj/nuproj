9ddda13d194e45a5aa445fe14de0b13c7e4c25d8
Immo Landwerth <immo@landwerth.net>
2014-09-28 18:36:47 -0700

    Ensure errors in package restore are detected
    We want to throw an exception when package restore fails.

--- src/NuProj.Tests/NuGetHelper.cs
+++ src/NuProj.Tests/NuGetHelper.cs
@@ -2,27 +2,24 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace NuProj.Tests
 {
     public static class NuGetHelper
     {
-        private static readonly object restoreTasksLock = new object();
-
-        private static readonly Dictionary<string, Task> restorePackagesTasks = new Dictionary<string, Task>(StringComparer.OrdinalIgnoreCase);
+        private static readonly object RestoreTasksLock = new object();
+        private static readonly Dictionary<string, Task> RestorePackagesTasks = new Dictionary<string, Task>(StringComparer.OrdinalIgnoreCase);
 
         public static Task RestorePackagesAsync(string path)
         {
             Task restorePackagesTask;
-            lock (restoreTasksLock)
+            lock (RestoreTasksLock)
             {
-                if (!restorePackagesTasks.TryGetValue(path, out restorePackagesTask))
+                if (!RestorePackagesTasks.TryGetValue(path, out restorePackagesTask))
                 {
                     restorePackagesTask = NuGetExeRestoreAsync(path);
-                    restorePackagesTasks[path] = restorePackagesTask;
+                    RestorePackagesTasks[path] = restorePackagesTask;
                 }
             }
 
@@ -32,25 +29,58 @@ namespace NuProj.Tests
         private static Task<int> NuGetExeRestoreAsync(string path)
         {
             var testOutDir = Directory.GetCurrentDirectory();
-            ProcessStartInfo startInfo = new ProcessStartInfo()
+
+            var startInfo = new ProcessStartInfo
             {
                 FileName = Path.Combine(testOutDir, "nuget.exe"),
                 Arguments = "restore",
                 WorkingDirectory = path,
                 UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardError = true,
+                RedirectStandardOutput = true
             };
 
             var process = new Process();
             process.StartInfo = startInfo;
             process.EnableRaisingEvents = true;
-            process.Start();
+
+            var outputLines = new List<string>();
+
+            process.ErrorDataReceived += (s, e) =>
+            {
+                lock (outputLines)
+                    outputLines.Add(e.Data);
+            };
+            process.OutputDataReceived += (s, e) =>
+            {
+                lock (outputLines)
+                    outputLines.Add(e.Data);
+            };
 
             var tcs = new TaskCompletionSource<int>();
-            process.Exited += (sender, args) => tcs.TrySetResult(process.ExitCode);
-            if (process.HasExited)
+            process.Exited += (sender, args) =>
             {
-                tcs.TrySetResult(process.ExitCode);
-            }
+                try
+                {
+                    if (process.ExitCode != 0)
+                    {
+                        var output = String.Join(Environment.NewLine, outputLines);
+                        var message = String.Format("NuGet package restore failed.{0}{1}", Environment.NewLine, output);
+                        tcs.SetException(new Exception(message));
+                    }
+
+                    tcs.SetResult(process.ExitCode);
+                }
+                finally
+                {
+                    process.Dispose();                   
+                }
+            };
+
+            process.Start();
+            process.BeginErrorReadLine();
+            process.BeginOutputReadLine();
 
             return tcs.Task;
         }
