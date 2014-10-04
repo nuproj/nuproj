c0afe2371ff0eae00ef015eb83849659c3e0332a
Immo Landwerth <immo@landwerth.net>
2014-10-04 16:38:49 -0700

    Allow scenario class to take properties

--- src/NuProj.Tests/Infrastructure/Scenario.cs
+++ src/NuProj.Tests/Infrastructure/Scenario.cs
@@ -10,20 +10,20 @@ namespace NuProj.Tests.Infrastructure
 {
     public static class Scenario
     {
-        public static async Task<IPackage> RestoreAndBuildSinglePackage(string scenarioName)
+        public static async Task<IPackage> RestoreAndBuildSinglePackage(string scenarioName, IDictionary<string, string> properties = null)
         {
-            var packages = await RestoreAndBuildPackages(scenarioName);
+            var packages = await RestoreAndBuildPackages(scenarioName, properties);
             return packages.Single();
         }
 
-        public static async Task<IReadOnlyCollection<IPackage>> RestoreAndBuildPackages(string scenarioName)
+        public static async Task<IReadOnlyCollection<IPackage>> RestoreAndBuildPackages(string scenarioName, IDictionary<string, string> properties = null)
         {
             var projectFullPath = Assets.GetScenarioSolutionPath(scenarioName);
 
             var projectDirectory = Path.GetDirectoryName(projectFullPath);
             await NuGetHelper.RestorePackagesAsync(projectDirectory);
-            
-            var result = await MSBuild.RebuildAsync(projectFullPath);
+
+            var result = await MSBuild.RebuildAsync(projectFullPath, properties);
             result.AssertSuccessfulBuild();
 
             return NuPkg.GetPackages(projectDirectory);
