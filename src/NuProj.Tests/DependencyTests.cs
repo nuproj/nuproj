954eefe933fd809315019fb9f4ce8bcfafbee7d4
kovalikp <kovalikp@gmail.com>
2014-10-27 21:39:44 +0100

    Assign target framework to NuProj dependencies. Fix issue #12.

--- src/NuProj.Tests/DependencyTests.cs
+++ src/NuProj.Tests/DependencyTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using Microsoft.Build.Framework;
 using NuGet;
@@ -80,13 +82,43 @@ namespace NuProj.Tests
             Assert.Equal(expectedVersions, versionSpecs);
         }
 
+        [Fact]
+        public async Task Dependency_MultipleFrameworks_AreResolved()
+        {
+            var package = await Scenario.RestoreAndBuildSinglePackage(
+                "Dependency_MultipleFrameworks_AreResolved", 
+                "Dependent");
+            var dependencySet = new []{
+                new PackageDependencySet(VersionUtility.ParseFrameworkName("net40"), new List<PackageDependency>
+                    {
+                        new PackageDependency("Dependency", new VersionSpec
+                            {
+                                IsMinInclusive = true,
+                                MinVersion = new SemanticVersion("1.0.0")
+                            })
+                    }),
+                new PackageDependencySet(VersionUtility.ParseFrameworkName("net45"), new List<PackageDependency>
+                    {
+                        new PackageDependency("Dependency", new VersionSpec
+                            {
+                                IsMinInclusive = true,
+                                MinVersion = new SemanticVersion("1.0.0")
+                            })
+                    }),
+            };
+
+            Assert.Equal(dependencySet, package.DependencySets, PackageDependencySetComparer.Instance);
+        }
+
         [Theory]
         [InlineData("Build")]
         [InlineData("Clean")]
         [InlineData("Rebuild")]
         public async Task Dependency_IsBuilt_WhenNotBuildingInsideVisualStudio(string target)
         {
-            var projectPath = Assets.GetScenarioFilePath("Dependency_IsBuilt_WhenNotBuildingInsideVisualStudio", @"NuGetPackage\NuGetPackage.nuproj");
+            var projectPath = Assets.GetScenarioFilePath(
+                "Dependency_IsBuilt_WhenNotBuildingInsideVisualStudio",
+                @"NuGetPackage\NuGetPackage.nuproj");
             var properties = MSBuild.Properties.Default;
             var result = await MSBuild.ExecuteAsync(projectPath, target, properties);
             result.AssertSuccessfulBuild();
@@ -112,7 +144,9 @@ namespace NuProj.Tests
         [InlineData("Rebuild")]
         public async Task Dependency_IsNotBuilt_WhenBuildingInsideVisualStudio(string target)
         {
-            var projectPath = Assets.GetScenarioFilePath("Dependency_IsNotBuilt_WhenBuildingInsideVisualStudio", @"NuGetPackage\NuGetPackage.nuproj");
+            var projectPath = Assets.GetScenarioFilePath(
+                "Dependency_IsNotBuilt_WhenBuildingInsideVisualStudio",
+                @"NuGetPackage\NuGetPackage.nuproj");
             var properties = MSBuild.Properties.Default.AddRange(MSBuild.Properties.BuildingInsideVisualStudio);
             var result = await MSBuild.ExecuteAsync(projectPath, target, properties);
             result.AssertSuccessfulBuild();
---
+++ src/NuProj.Tests/Infrastructure/PackageDependencySetComparer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NuGet;
+
+namespace NuProj.Tests.Infrastructure
+{
+    public class PackageDependencySetComparer : IEqualityComparer<PackageDependencySet>
+    {
+        private static readonly StringComparer _stringComparer = StringComparer.OrdinalIgnoreCase;
+        private static readonly PackageDependencySetComparer _instance = new PackageDependencySetComparer();
+
+        public static PackageDependencySetComparer Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+
+        public bool Equals(PackageDependencySet x, PackageDependencySet y)
+        {
+            if (x == null && x == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            var xDependencies = new HashSet<PackageDependency>(
+                x.Dependencies ?? Enumerable.Empty<PackageDependency>(),
+                PackageDependencyComparer.Instance);
+
+            var yDependencies = new HashSet<PackageDependency>(
+                y.Dependencies ?? Enumerable.Empty<PackageDependency>(),
+                PackageDependencyComparer.Instance);
+
+            return x.TargetFramework == y.TargetFramework
+                && xDependencies.SetEquals(yDependencies);
+        }
+
+        public int GetHashCode(PackageDependencySet obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException("obj");
+            }
+
+            return obj.TargetFramework == null ? 0 : obj.TargetFramework.GetHashCode();
+        }
+    }
+}
