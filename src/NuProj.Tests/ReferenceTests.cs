53188b3414375d7b4935a1ff6fc927b898a41c00
kovalikp <kovalikp@gmail.com>
2015-04-06 17:26:25 +0200

    Resolved issue when Tool assembly had NuGet dependency.

--- src/NuProj.Tests/ReferenceTests.cs
+++ src/NuProj.Tests/ReferenceTests.cs
@@ -45,18 +45,26 @@ namespace NuProj.Tests
         }
 
         [Theory]
-        [InlineData("PackageToBuild", new[] { @"build\Tool.dll" })]
-        [InlineData("PackageToLib", new[] { @"lib\net45\Tool.dll" })]
-        [InlineData("PackageToRoot", new[] { @"Tool.dll", @"Tool.pdb" })]
-        [InlineData("PackageToTools", new[] { @"tools\Tool.dll" })]
-        [InlineData("PackageDependencyToTools", new[] { @"tools\Tool.dll" })]
-        [InlineData("PackageClosureToTools", new[] { @"tools\Tool.dll", @"tools\ToolWithClosure.dll" })]
-        [InlineData("PackageToContent", new[] { @"content\Tool.dll", @"content\Tool.pdb" })]
-        public async Task References_PackageDirectory_ToolIsPackaged(string packageId, string[] expectedFiles)
+        [InlineData("PackageToBuild", new[] { @"build\Tool.dll" }, new string[0])]
+        [InlineData("PackageToLib", new[] { @"lib\net45\Tool.dll" }, new string[0])]
+        [InlineData("PackageToRoot", new[] { @"Tool.dll", @"Tool.pdb" }, new string[0])]
+        [InlineData("PackageToTools", new[] { @"tools\Tool.dll" }, new string[0])]
+        [InlineData("PackageDependencyToTools", new[] { @"tools\Tool.dll" }, new[] { "PackageToTools (>= 1.0.0)" })]
+        [InlineData("PackageClosureToTools", new[] { @"tools\Tool.dll", @"tools\ToolWithClosure.dll" }, new string[0])]
+        [InlineData("PackageToContent", new[] { @"content\Tool.dll", @"content\Tool.pdb" }, new string[0])]
+        [InlineData("PackageNuGetDependencyToTools", new[] { @"tools\System.Collections.Immutable.dll", @"tools\ToolWithDependency.dll" }, new string[0])]
+        public async Task References_PackageDirectory_ToolIsPackaged(
+            string packageId, 
+            string[] expectedFiles, 
+            string[] expectedDependencies)
         {
             var package = await Scenario.RestoreAndBuildSinglePackage("References_PackageDirectory", packageId);
-            var files = package.GetFiles().Select(f => f.Path);
-            Assert.Equal(expectedFiles.OrderBy(x => x), files.OrderBy(x => x));
+            var actualFiles = package.GetFiles().Select(f => f.Path).OrderBy(x => x);
+            var actualDependencies = package.DependencySets.NullAsEmpty().Flatten().OrderBy(x => x);
+            expectedFiles = expectedFiles.OrderBy(x => x).ToArray();
+            expectedDependencies = expectedDependencies.OrderBy(x => x).ToArray();
+            Assert.Equal(expectedFiles, actualFiles);
+            Assert.Equal(expectedDependencies, actualDependencies);
         }
     }
 }
