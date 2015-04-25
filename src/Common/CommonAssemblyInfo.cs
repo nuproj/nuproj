e81f89e3f0b49a6161da347321b4493dd496423c
Immo Landwerth <immo@landwerth.net>
2015-04-25 16:53:42 -0700

    Add note to change the version number for both VSIX files

--- src/Common/CommonAssemblyInfo.cs
+++ src/Common/CommonAssemblyInfo.cs
@@ -10,7 +10,10 @@
 [assembly : AssemblyMetadata("LicenseUrl", "http://nuproj.net/LICENSE")]
 
 // NOTE: When updating the version, you also need to update the Identity/@Version
-//       attribtute in src\NuProj.ProjectSystem.12\source.extension.vsixmanifest.
+//       attribtute in:
+//
+//          src\NuProj.ProjectSystem.12\source.extension.vsixmanifest
+//          src\NuProj.ProjectSystem.14\source.extension.vsixmanifest
 
 [assembly: AssemblyVersion("0.10.0.0")]
 [assembly: AssemblyFileVersion("0.10.0.0")]
