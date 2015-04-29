eb481c2a6520355e98dc8e8532f25eed4d046640
Andir <spam.andir@gmail.com>
2015-04-29 14:58:21 +0300

    Add failing test for packaged library with XML-doc enabled

---
+++ src/NuProj.Tests/CreatePackageTests.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using NuProj.Tests.Infrastructure;
+using Xunit;
+
+namespace NuProj.Tests
+{
+    public class CreatePackageTests
+    {
+        [Fact]
+        public async Task CreatePackage_LibraryWithXmlDoc_ShouldNotFail()
+        {
+            var solutionPath = Assets.GetScenarioSolutionPath("Dependency_NoDependencies_Fails");
+            var result = await MSBuild.RebuildAsync(solutionPath);
+
+            result.AssertSuccessfulBuild();
+        }
+    }
+}
