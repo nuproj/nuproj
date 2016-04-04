58f293d4a5a77d1b844113506f2cd54a46404437
Andrew Arnott <andrewarnott@gmail.com>
2016-04-03 18:17:52 -0700

    Add test for NuProj referencing dotnet 5.0 library

---
+++ src/NuProj.Tests/DotNetTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NuProj.Tests.Infrastructure;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace NuProj.Tests
+{
+    public class DotNetTests
+    {
+        private readonly ITestOutputHelper logger;
+
+        public DotNetTests(ITestOutputHelper logger)
+        {
+            this.logger = logger;
+        }
+
+        [Fact]
+        public async Task DotNet_CanPackage()
+        {
+             var package = await Scenario.RestoreAndBuildSinglePackageAsync(testLogger: logger);
+        }
+    }
+}
